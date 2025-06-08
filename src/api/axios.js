03e799e24c0ec83d536038c3acd514f145548565
gustavo <67645041g@gmail.com>
2025-06-08 09:35:29 -0400

    cambios inter

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios  from "axios";
-
+/* http://localhost:4000/api */
  const instance = axios.create({
     baseURL: 'https://todito.onrender.com/api', // ✅ URL de tu backend
     withCredentials: true,
